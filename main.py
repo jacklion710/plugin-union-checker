8acfebf9d5750093a89fc324aa46a9878a720dc0
jacklion710 <jack.lion710@gmail.com>
2024-03-24 23:59:03 -0400

    More steps planned

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # User can specify a custom directory for their plugin location
 # Recursively search the folders and add them to a list/dictionary
 # Result is a table or list showing each plugin and the formats that are shared
+# Display any common sense reminders such as that windows doesn't support audio units (AU) formats
 
 # Class for searching through folders and outputting a persons plugin profile
 # Class for comparing plugin profiles and outputting relevant statistics and data to aid collaborators in making decisions on using plugins, tools, etc...
