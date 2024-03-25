7c9cf62c8855101c52da0741b3b19259cf837cbe
jacklion710 <jack.lion710@gmail.com>
2024-03-24 23:45:17 -0400

    Project outline

---
+++ README.md
@@ -0,0 +1,5 @@
+# Plugin Union Checker
+
+Utility for electronic music collaborators to check what tools they both have and which they don't share to assist in maximizing compatibility
+
+Currently under development, check back soon
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # main.py
+
+# Develop for mac first then test on windows
+# Check users OS and display it
+# Only macOS users have access to AU plugins 
+# Identify most common file locations for plugins based on OS
+# User can specify a custom directory for their plugin location
+# Recursively search the folders and add them to a list/dictionary
+# Result is a table or list showing each plugin and the formats that are shared
+
+# Class for searching through folders and outputting a persons plugin profile
+# Class for comparing plugin profiles and outputting relevant statistics and data to aid collaborators in making decisions on using plugins, tools, etc...
+# GUI for ease of use
+# Unit tests
