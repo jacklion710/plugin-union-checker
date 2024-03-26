20995c2c3ae0a15cc84bf71a4c1784efcc319094
jacklion710 <jack.lion710@gmail.com>
2024-03-26 16:26:24 -0400

    Moved ideas for later into a markdown file

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 # main.py
 from graphics import Window
 
-# Ideas for later
-# User can specify a custom directory for their plugin locatio
-# Display any common sense reminders such as that windows doesn't support audio units (AU) formats
-# Checkboxes for determining which folders to visit
-# Detect what DAWs are present
-
 def main():
     window = Window()
     window.mainloop()
---
+++ notes.md
@@ -0,0 +1,6 @@
+# Ideas for later
+
+* User can specify a custom directory for their plugin location
+* Display any common sense reminders such as that windows doesn't support audio units (AU) formats
+* Checkboxes for determining which folders to visit
+* Detect what DAWs are present
