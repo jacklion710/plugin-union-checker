738723dfa7b0dff92d2bdb4c9f478ebb22ac943b
jacklion710 <jack.lion710@gmail.com>
2024-03-26 16:25:18 -0400

    Idea for

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from graphics import Window
 # User can specify a custom directory for their plugin locatio
 # Display any common sense reminders such as that windows doesn't support audio units (AU) formats
 # Checkboxes for determining which folders to visit
+# Detect what DAWs are present
 
 def main():
     window = Window()
