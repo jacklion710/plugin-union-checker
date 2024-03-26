e48a03e207bd29ab8c5d4a4ff08fbe38bd1c1e0e
jacklion710 <jack.lion710@gmail.com>
2024-03-26 12:43:49 -0400

    Call window in mainloop

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 # main.py
+from graphics import Window
 
-# Develop for mac first then test on windows
-# Check users OS and display it
-# Only macOS users have access to AU plugins 
-# Identify most common file locations for plugins based on OS
-# User can specify a custom directory for their plugin location
-# Recursively search the folders and add them to a list/dictionary
-# Result is a table or list showing each plugin and the formats that are shared
+# Ideas for later
+# User can specify a custom directory for their plugin locatio
 # Display any common sense reminders such as that windows doesn't support audio units (AU) formats
 
-# Class for searching through folders and outputting a persons plugin profile
-# Class for comparing plugin profiles and outputting relevant statistics and data to aid collaborators in making decisions on using plugins, tools, etc...
-# GUI for ease of use
-# Unit tests
-
 def main():
-    pass
+    window = Window()
+    window.mainloop()
 
 if __name__ == "__main__":
     main()
